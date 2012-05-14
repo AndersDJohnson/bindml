3a20df2fa8f9bd9e583037387beaf436574dbb30
anders <anders@debian.Belkin>
2012-05-14 13:08:32 -0500

    add universal module support

--- build/bindml.js
+++ build/bindml.js
@@ -1,194 +1,197 @@
 (function() {
-  var cheerio, fs, jsonPath, register, render, soupselect, templates, tokens, _;
 
-  cheerio = require("cheerio-AndersDJohnson");
-
-  soupselect = require("cheerio-soupselect");
-
-  fs = require("promised-io/fs");
-
-  jsonPath = require("JSONPath");
-
-  _ = require("underscore");
-
-  tokens = {
-    local: '$$'
-  };
-
-  templates = {};
-
-  module.exports.register = exports.register = register = function(name, template) {
-    return templates[name] = template;
-  };
-
-  module.exports.render = exports.render = render = function(name, data, opts) {
-    var $, $top, doClasses, doIncludes, doScopes, fillAttrs, getAttr, getAttrs, keyToValue, resolveJSONPaths;
-    if (opts == null) opts = {};
-    opts = _.defaults(opts, {
-      tidy: false
-    });
-    $ = cheerio.load(templates[name]);
-    keyToValue = function(key) {
-      var expr, results;
-      expr = "$." + key;
-      results = jsonPath.eval(data, expr);
-      return results;
-    };
-    $top = $($.dom());
-    resolveJSONPaths = function(paths) {
-      var path, resolved, _i, _len;
-      resolved = [];
-      for (_i = 0, _len = paths.length; _i < _len; _i++) {
-        path = paths[_i];
-        path = path.replace(/^\$\$/, '');
-        resolved.push(path);
-      }
-      return resolved.join("");
-    };
-    getAttrs = function($elem) {
-      var attr, attrs, elem, _i, _len, _ref;
-      elem = $elem.get(0);
-      attrs = {};
-      if (elem.attributes != null) {
-        _ref = elem.attributes;
-        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-          attr = _ref[_i];
-          attrs[attr.name] = attr.value;
-        }
-      } else if (elem.attribs != null) {
-        attrs = elem.attribs;
-      }
-      return attrs;
-    };
-    getAttr = function($elem) {
-      if ($elem.attr("data-bind") != null) {
-        return "data-bind";
-      } else if ($elem.attr("data-with") != null) {
-        return "data-with";
-      } else if ($elem.attr("data-each") != null) {
-        return "data-each";
-      }
-      return false;
-    };
-    doIncludes = function($context) {
-      var $includes;
-      $includes = $("[data-include]", $context);
-      return $includes.each(function(index, elem) {
-        var $elem, $included, included, key, wrapper;
-        $elem = $(elem);
-        key = $elem.attr("data-include");
-        included = templates[key];
-        if (included != null) {
-          wrapper = "<div data-partial-wrapper>" + included + "</div>";
-          $included = $(wrapper);
-          $elem.replaceWith($included.html());
-          return doIncludes($included);
-        }
-      });
-    };
-    fillAttrs = function($elem, scope) {
-      var attrs, key, match, subscope, val, value;
-      attrs = getAttrs($elem);
-      for (key in attrs) {
-        value = attrs[key];
-        match = key.match(/^data-attr-(.+)$/);
-        if (match != null) {
-          subscope = scope;
-          if (scope !== "") subscope += ".";
-          subscope += value;
-          val = keyToValue(subscope)[0];
-          $elem.attr(match[1], val);
-          $elem.removeAttr(match[0]);
-        }
-      }
-      return $elem.removeAttr("data-attr");
-    };
-    doClasses = function($elem, scope) {
-      var falseClass, match, property, subscope, trueClass, val, value, values, _i, _len;
-      value = $elem.attr("data-classes");
-      values = value.split(/\s+/);
-      for (_i = 0, _len = values.length; _i < _len; _i++) {
-        value = values[_i];
-        match = value.match(/^(.*?)(\?(.*?)(\:(.*))?)?$/);
-        if (match != null) {
-          property = match[1];
-          subscope = scope;
-          if (scope !== "") subscope += ".";
-          subscope += property;
-          val = keyToValue(subscope)[0];
-          trueClass = match[3];
-          falseClass = match[5];
-          if (val) {
-            if (trueClass != null) {
-              $elem.addClass(trueClass);
-            } else {
-              $elem.addClass(value[0]);
+  (function() {
+    var cheerio, exports, factory, jsonPath, name, _;
+    name = "bindml";
+    factory = function($, _, jsonPath) {
+      var register, render, templates, tokens;
+      tokens = {
+        local: '$$'
+      };
+      templates = {};
+      module.exports.register = exports.register = register = function(name, template) {
+        return templates[name] = template;
+      };
+      return module.exports.render = exports.render = render = function(name, data, opts) {
+        var $top, doClasses, doIncludes, doScopes, fillAttrs, getAttr, getAttrs, keyToValue, resolveJSONPaths;
+        if (opts == null) opts = {};
+        opts = _.defaults(opts, {
+          tidy: false
+        });
+        $ = cheerio.load(templates[name]);
+        keyToValue = function(key) {
+          var expr, results;
+          expr = "$." + key;
+          results = jsonPath.eval(data, expr);
+          return results;
+        };
+        $top = $($.dom());
+        resolveJSONPaths = function(paths) {
+          var path, resolved, _i, _len;
+          resolved = [];
+          for (_i = 0, _len = paths.length; _i < _len; _i++) {
+            path = paths[_i];
+            path = path.replace(/^\$\$/, '');
+            resolved.push(path);
+          }
+          return resolved.join("");
+        };
+        getAttrs = function($elem) {
+          var attr, attrs, elem, _i, _len, _ref;
+          elem = $elem.get(0);
+          attrs = {};
+          if (elem.attributes != null) {
+            _ref = elem.attributes;
+            for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+              attr = _ref[_i];
+              attrs[attr.name] = attr.value;
             }
-          } else if (falseClass != null) {
-            $elem.addClass(falseClass);
+          } else if (elem.attribs != null) {
+            attrs = elem.attribs;
           }
-        }
-      }
-      return $elem.removeAttr("data-classes");
-    };
-    doScopes = function($contexts, scope) {
-      var scopeSelector;
-      if (scope == null) scope = "";
-      scopeSelector = "[data-with], [data-each], [data-bind], [data-attr], [data-classes]";
-      return $contexts.each(function(index, elem) {
-        var $context, $do, attr, subscope, val, _results;
-        $context = $(elem);
-        $do = $context.find(scopeSelector).not("[data-processed]").first();
-        _results = [];
-        while ($do.size() > 0) {
-          attr = getAttr($do);
-          subscope = scope;
-          if (scope !== "") subscope += ".";
-          subscope += $do.attr(attr);
-          if ($do.attr("data-attr") != null) fillAttrs($do, scope);
-          if ($do.attr("data-classes") != null) doClasses($do, scope);
-          $do.attr("data-processed", "data-processed");
-          switch (attr) {
-            case "data-each":
-              val = keyToValue(subscope)[0];
-              _.each(val, function(elem, index) {
-                var $doClone, subscopei;
-                $doClone = $do.clone();
-                $doClone.attr("data-each", subscope);
-                subscopei = subscope + ("[" + index + "]");
-                $doClone.attr("data-bind", subscopei);
-                fillAttrs($doClone, subscopei);
-                $doClone.attr("data-processed", "data-processed");
-                doScopes($doClone.children(), subscopei);
-                return $do.before($doClone);
-              });
-              $do.remove();
-              break;
-            case "data-bind":
-              val = keyToValue(subscope)[0];
-              $do.html(val);
-              $do.attr("data-bind", subscope);
-              fillAttrs($do, subscope);
-              doScopes($do.children(), subscope);
-              break;
-            case "data-with":
-              $do.attr(attr, subscope);
-              fillAttrs($do, subscope);
-              doScopes($do.children(), subscope);
+          return attrs;
+        };
+        getAttr = function($elem) {
+          if ($elem.attr("data-bind") != null) {
+            return "data-bind";
+          } else if ($elem.attr("data-with") != null) {
+            return "data-with";
+          } else if ($elem.attr("data-each") != null) {
+            return "data-each";
           }
-          _results.push($do = $context.find(scopeSelector).not("[data-processed]").first());
+          return false;
+        };
+        doIncludes = function($context) {
+          var $includes;
+          $includes = $("[data-include]", $context);
+          return $includes.each(function(index, elem) {
+            var $elem, $included, included, key, wrapper;
+            $elem = $(elem);
+            key = $elem.attr("data-include");
+            included = templates[key];
+            if (included != null) {
+              wrapper = "<div data-partial-wrapper>" + included + "</div>";
+              $included = $(wrapper);
+              $elem.replaceWith($included.html());
+              return doIncludes($included);
+            }
+          });
+        };
+        fillAttrs = function($elem, scope) {
+          var attrs, key, match, subscope, val, value;
+          attrs = getAttrs($elem);
+          for (key in attrs) {
+            value = attrs[key];
+            match = key.match(/^data-attr-(.+)$/);
+            if (match != null) {
+              subscope = scope;
+              if (scope !== "") subscope += ".";
+              subscope += value;
+              val = keyToValue(subscope)[0];
+              $elem.attr(match[1], val);
+              $elem.removeAttr(match[0]);
+            }
+          }
+          return $elem.removeAttr("data-attr");
+        };
+        doClasses = function($elem, scope) {
+          var falseClass, match, property, subscope, trueClass, val, value, values, _i, _len;
+          value = $elem.attr("data-classes");
+          values = value.split(/\s+/);
+          for (_i = 0, _len = values.length; _i < _len; _i++) {
+            value = values[_i];
+            match = value.match(/^(.*?)(\?(.*?)(\:(.*))?)?$/);
+            if (match != null) {
+              property = match[1];
+              subscope = scope;
+              if (scope !== "") subscope += ".";
+              subscope += property;
+              val = keyToValue(subscope)[0];
+              trueClass = match[3];
+              falseClass = match[5];
+              if (val) {
+                if (trueClass != null) {
+                  $elem.addClass(trueClass);
+                } else {
+                  $elem.addClass(value[0]);
+                }
+              } else if (falseClass != null) {
+                $elem.addClass(falseClass);
+              }
+            }
+          }
+          return $elem.removeAttr("data-classes");
+        };
+        doScopes = function($contexts, scope) {
+          var scopeSelector;
+          if (scope == null) scope = "";
+          scopeSelector = "[data-with], [data-each], [data-bind], [data-attr], [data-classes]";
+          return $contexts.each(function(index, elem) {
+            var $context, $do, attr, subscope, val, _results;
+            $context = $(elem);
+            $do = $context.find(scopeSelector).not("[data-processed]").first();
+            _results = [];
+            while ($do.size() > 0) {
+              attr = getAttr($do);
+              subscope = scope;
+              if (scope !== "") subscope += ".";
+              subscope += $do.attr(attr);
+              if ($do.attr("data-attr") != null) fillAttrs($do, scope);
+              if ($do.attr("data-classes") != null) doClasses($do, scope);
+              $do.attr("data-processed", "data-processed");
+              switch (attr) {
+                case "data-each":
+                  val = keyToValue(subscope)[0];
+                  _.each(val, function(elem, index) {
+                    var $doClone, subscopei;
+                    $doClone = $do.clone();
+                    $doClone.attr("data-each", subscope);
+                    subscopei = subscope + ("[" + index + "]");
+                    $doClone.attr("data-bind", subscopei);
+                    fillAttrs($doClone, subscopei);
+                    $doClone.attr("data-processed", "data-processed");
+                    doScopes($doClone.children(), subscopei);
+                    return $do.before($doClone);
+                  });
+                  $do.remove();
+                  break;
+                case "data-bind":
+                  val = keyToValue(subscope)[0];
+                  $do.html(val);
+                  $do.attr("data-bind", subscope);
+                  fillAttrs($do, subscope);
+                  doScopes($do.children(), subscope);
+                  break;
+                case "data-with":
+                  $do.attr(attr, subscope);
+                  fillAttrs($do, subscope);
+                  doScopes($do.children(), subscope);
+              }
+              _results.push($do = $context.find(scopeSelector).not("[data-processed]").first());
+            }
+            return _results;
+          });
+        };
+        doIncludes($top);
+        doScopes($top);
+        $("[data-processed]").removeAttr("data-processed");
+        if (opts.tidy && ($.tidy != null)) {
+          return $.tidy();
+        } else {
+          return $.html();
         }
-        return _results;
-      });
+      };
     };
-    doIncludes($top);
-    doScopes($top);
-    $("[data-processed]").removeAttr("data-processed");
-    if (opts.tidy) {
-      return $.tidy();
-    } else {
-      return $.html();
+    if (typeof define !== "undefined" && define !== null) {
+      return define(["jquery", "underscore", "jsonPath"], factory);
+    } else if ((typeof module !== "undefined" && module !== null ? module.exports : void 0) != null) {
+      cheerio = require("cheerio-AndersDJohnson");
+      jsonPath = require("JSONPath");
+      _ = require("underscore");
+      return module.exports = exports = factory(cheerio, _, jsonPath);
+    } else if (typeof window !== "undefined" && window !== null) {
+      return window[name] = factory(jQuery, _, jsonPath);
     }
-  };
+  })();
 
 }).call(this);
--- src/bindml.coffee
+++ src/bindml.coffee
@@ -1,163 +1,175 @@
-cheerio = require "cheerio-AndersDJohnson"
-soupselect = require "cheerio-soupselect"
-fs = require "promised-io/fs"
-jsonPath = require "JSONPath"
-_ = require "underscore"
+do ->
+	name = "bindml"
+	
+	factory = ($, _, jsonPath) ->
+		
+		tokens =
+			local: '$$'
 
-tokens =
-	local: '$$'
+		templates = {}
+		module.exports.register = exports.register = register = (name, template) ->
+			templates[name] = template
 
-templates = {}
-module.exports.register = exports.register = register = (name, template) ->
-	templates[name] = template
-
-module.exports.render = exports.render = render = (name, data, opts = {}) ->
+		module.exports.render = exports.render = render = (name, data, opts = {}) ->
 	
-	opts = _.defaults opts, {
-		tidy: false
-	}
+			opts = _.defaults opts, {
+				tidy: false
+			}
 	
-	$ = cheerio.load templates[name]
+			$ = cheerio.load templates[name]
 	
-	keyToValue = (key) ->
-		expr = "$.#{key}"
-		results = jsonPath.eval data, expr
-		return results
+			keyToValue = (key) ->
+				expr = "$.#{key}"
+				results = jsonPath.eval data, expr
+				return results
 	
-	$top = $($.dom())
+			$top = $($.dom())
 	
-	resolveJSONPaths = (paths) ->
-		resolved = []
-		for path in paths
-			path = path.replace /^\$\$/, ''
-			resolved.push path
-		return resolved.join ""
+			resolveJSONPaths = (paths) ->
+				resolved = []
+				for path in paths
+					path = path.replace /^\$\$/, ''
+					resolved.push path
+				return resolved.join ""
 	
-	getAttrs = ($elem) ->
-		elem = $elem.get(0)
-		attrs = {}
-		if elem.attributes?
-			for attr in elem.attributes
-				attrs[attr.name] = attr.value
-		else if elem.attribs?
-			attrs = elem.attribs
-		return attrs
+			getAttrs = ($elem) ->
+				elem = $elem.get(0)
+				attrs = {}
+				if elem.attributes?
+					for attr in elem.attributes
+						attrs[attr.name] = attr.value
+				else if elem.attribs?
+					attrs = elem.attribs
+				return attrs
 	
-	getAttr = ($elem) ->
-		if $elem.attr("data-bind")?
-			return "data-bind"
-		else if $elem.attr("data-with")?
-			return "data-with"
-		else if $elem.attr("data-each")?
-			return "data-each"
-		#else if $elem.attr("data-attr")?
-		#	return "data-attr"
-		return false
+			getAttr = ($elem) ->
+				if $elem.attr("data-bind")?
+					return "data-bind"
+				else if $elem.attr("data-with")?
+					return "data-with"
+				else if $elem.attr("data-each")?
+					return "data-each"
+				#else if $elem.attr("data-attr")?
+				#	return "data-attr"
+				return false
 	
-	doIncludes = ($context) ->
-		$includes = $("[data-include]", $context)
-		$includes.each((index, elem) ->
-			$elem = $(elem)
-			key = $elem.attr("data-include")
-			included = templates[key]
-			if included?
-				wrapper = "<div data-partial-wrapper>#{included}</div>"
-				$included = $(wrapper)
-				$elem.replaceWith $included.html()
-				doIncludes $included
-		)
+			doIncludes = ($context) ->
+				$includes = $("[data-include]", $context)
+				$includes.each((index, elem) ->
+					$elem = $(elem)
+					key = $elem.attr("data-include")
+					included = templates[key]
+					if included?
+						wrapper = "<div data-partial-wrapper>#{included}</div>"
+						$included = $(wrapper)
+						$elem.replaceWith $included.html()
+						doIncludes $included
+				)
 	
-	fillAttrs = ($elem, scope) ->
-		attrs = getAttrs($elem)
-		for key, value of attrs
-			match = key.match /^data-attr-(.+)$/
-			if match?
-				subscope = scope
-				subscope += "." if scope isnt ""
-				subscope += value
-				val = keyToValue(subscope)[0]
-				$elem.attr(match[1], val)
-				$elem.removeAttr(match[0])
-		$elem.removeAttr("data-attr")
-	
-	doClasses = ($elem, scope) ->
-		value = $elem.attr("data-classes")
-		values = value.split /\s+/
-		for value in values
-			match = value.match /^(.*?)(\?(.*?)(\:(.*))?)?$/
-			if match?
-				property = match[1]
-				subscope = scope
-				subscope += "." if scope isnt ""
-				subscope += property
-				val = keyToValue(subscope)[0]
-				trueClass = match[3]
-				falseClass = match[5]
-				if val
-					if trueClass?
-						$elem.addClass trueClass
-					else
-						$elem.addClass value[0]
-				else if falseClass?
-					$elem.addClass falseClass
-		$elem.removeAttr("data-classes")
-	
-	doScopes = ($contexts, scope="") ->
-		scopeSelector = "[data-with], [data-each], [data-bind], [data-attr], [data-classes]"
-		$contexts.each((index, elem) ->
-			$context = $(elem)
-			$do = $context.find(scopeSelector).not("[data-processed]").first()
-			while $do.size() > 0
-				
-				attr = getAttr($do)
-				subscope = scope
-				subscope += "." if scope isnt ""
-				subscope += $do.attr(attr)
-				
-				if $do.attr("data-attr")?
-					fillAttrs($do, scope)
-				
-				if $do.attr("data-classes")?
-					doClasses($do, scope)
-				
-				$do.attr("data-processed", "data-processed")
-				switch attr
-					when "data-each"
+			fillAttrs = ($elem, scope) ->
+				attrs = getAttrs($elem)
+				for key, value of attrs
+					match = key.match /^data-attr-(.+)$/
+					if match?
+						subscope = scope
+						subscope += "." if scope isnt ""
+						subscope += value
 						val = keyToValue(subscope)[0]
-						_.each(val, (elem, index) ->
-							$doClone = $do.clone()
-							$doClone.attr("data-each", subscope)
-							subscopei = subscope + "[#{index}]"
-							$doClone.attr("data-bind", subscopei)
-							fillAttrs($doClone, subscopei)
-							$doClone.attr("data-processed", "data-processed")
-							doScopes($doClone.children(), subscopei)
-							$do.before($doClone)
-						)
-						$do.remove()
-					when "data-bind"
+						$elem.attr(match[1], val)
+						$elem.removeAttr(match[0])
+				$elem.removeAttr("data-attr")
+	
+			doClasses = ($elem, scope) ->
+				value = $elem.attr("data-classes")
+				values = value.split /\s+/
+				for value in values
+					match = value.match /^(.*?)(\?(.*?)(\:(.*))?)?$/
+					if match?
+						property = match[1]
+						subscope = scope
+						subscope += "." if scope isnt ""
+						subscope += property
 						val = keyToValue(subscope)[0]
-						$do.html(val)
-						$do.attr("data-bind", subscope)
-						fillAttrs($do, subscope)
-						doScopes($do.children(), subscope)
-					when "data-with"
-						$do.attr(attr, subscope)
-						fillAttrs($do, subscope)
-						doScopes($do.children(), subscope)
+						trueClass = match[3]
+						falseClass = match[5]
+						if val
+							if trueClass?
+								$elem.addClass trueClass
+							else
+								$elem.addClass value[0]
+						else if falseClass?
+							$elem.addClass falseClass
+				$elem.removeAttr("data-classes")
+	
+			doScopes = ($contexts, scope="") ->
+				scopeSelector = "[data-with], [data-each], [data-bind], [data-attr], [data-classes]"
+				$contexts.each((index, elem) ->
+					$context = $(elem)
+					$do = $context.find(scopeSelector).not("[data-processed]").first()
+					while $do.size() > 0
+				
+						attr = getAttr($do)
+						subscope = scope
+						subscope += "." if scope isnt ""
+						subscope += $do.attr(attr)
+				
+						if $do.attr("data-attr")?
+							fillAttrs($do, scope)
+				
+						if $do.attr("data-classes")?
+							doClasses($do, scope)
+				
+						$do.attr("data-processed", "data-processed")
+						switch attr
+							when "data-each"
+								val = keyToValue(subscope)[0]
+								_.each(val, (elem, index) ->
+									$doClone = $do.clone()
+									$doClone.attr("data-each", subscope)
+									subscopei = subscope + "[#{index}]"
+									$doClone.attr("data-bind", subscopei)
+									fillAttrs($doClone, subscopei)
+									$doClone.attr("data-processed", "data-processed")
+									doScopes($doClone.children(), subscopei)
+									$do.before($doClone)
+								)
+								$do.remove()
+							when "data-bind"
+								val = keyToValue(subscope)[0]
+								$do.html(val)
+								$do.attr("data-bind", subscope)
+								fillAttrs($do, subscope)
+								doScopes($do.children(), subscope)
+							when "data-with"
+								$do.attr(attr, subscope)
+								fillAttrs($do, subscope)
+								doScopes($do.children(), subscope)
 				
 				
 				
-				$do = $context.find(scopeSelector).not("[data-processed]").first()
-		)
+						$do = $context.find(scopeSelector).not("[data-processed]").first()
+				)
 	
-	doIncludes $top
-	doScopes $top
+			doIncludes $top
+			doScopes $top
 	
-	$("[data-processed]").removeAttr("data-processed")
+			$("[data-processed]").removeAttr("data-processed")
 	
-	if opts.tidy
-		return $.tidy()
-	else
-		return $.html()
+			if opts.tidy and $.tidy?
+				return $.tidy()
+			else
+				return $.html()
+	
+	# universal module wrapper (CommonJS, AMD/RequireJS, and plain browser support)
+	if define?
+		define ["jquery", "underscore", "jsonPath"], factory
+	else if module?.exports?
+		cheerio = require "cheerio-AndersDJohnson"
+		#soupselect = require "cheerio-soupselect"
+		jsonPath = require "JSONPath"
+		_ = require "underscore"
+		module.exports = exports = factory(cheerio, _, jsonPath)
+	else if window?
+		window[name] = factory(jQuery, _, jsonPath)
+	
 
